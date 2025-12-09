68f43499e616227df00d2979ac46b79d6229d12d
Patrik Šlárka <patrik@slarka.com>
2025-12-09 13:18:26 +0100

    Fix CORS

--- apps/be/src/main.ts
+++ apps/be/src/main.ts
@@ -4,7 +4,10 @@ import { txtToJson } from './txt-to-json';
 const API_URL =
   'https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt';
 const PORT = process.env.PORT ?? 1337;
-const HEADERS = { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*.slarka.com' };
+const HEADERS = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': 'https://momence-assignment.slarka.com',
+};
 
 const server = createServer(async (_req, res) => {
   const response = await fetch(API_URL);
